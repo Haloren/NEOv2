d17f49bec5089a46b7af6f11842ee0da01d1c817
Haloren <mckennakyler@gmail.com>
2020-09-03 18:29:52 -0700

    added functions for countdown and to display countdown

---
+++ script.js
@@ -0,0 +1,33 @@
+const d = document.getElementById('days');
+const h = document.getElementById('hours');
+const m = document.getElementById('minutes');
+const s = document.getElementById('seconds');
+
+// Nearest approach astroid date should populate here
+const nearestApproach = "1 Jan 2021"; 
+
+function countdown() {
+    const nearestDate = new Date(nearestApproach);
+    const currentDate = new Date();
+
+    const totalSeconds = (nearestDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    // console.log(days, hours, minutes, seconds);
+
+    d.innerHTML = formatTime(days);
+    h.innerHTML = formatTime(hours);
+    m.innerHTML = formatTime(minutes);
+    s.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? "0"+time : time;
+}
+
+countdown();
+setInterval(countdown, 1000); //countdown by 1 second
